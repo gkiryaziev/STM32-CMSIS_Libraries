a10ae4241d74fd410d33a7a91e9333a43b348dd2
gkiryaziev <linko@ukr.net>
2019-06-08 18:34:56 +0300

    [FIX] accel converting to G

--- Lib/LSM303DLHC/LSM303DLHC.c
+++ Lib/LSM303DLHC/LSM303DLHC.c
@@ -4,6 +4,18 @@
  *  Created on: 25.05.2019
  *      Author: Admin
  *
+ *  Accelerometer: one 12-bit reading (left-justified)  per axis
+ *  Magnetometer : one 12-bit reading (right-justified) per axis
+ *
+ *  - The accelerometer and magnetometer both output readings in a 16-bit format
+ *  (obtained by combining the values in two 8-bit registers for each axis),
+ *  but they contain a maximum of 12 bits of precision. For the accelerometer,
+ *  at least the lowest 4 bits of the output values are always 0, and for the
+ *  magnetometer, the highest 4 bits of the output values are always 0.
+ *  - The accelerometer gives low-resolution 10-bit readings by default
+ *  (the lowest 6 bits of the output are always 0). To get the full 12-bit
+ *  resolution, you must set the HR (high resolution) bit in the CTRL_REG4_A register.
+ *
  *  SCL1	- PB6
  *  SDA1	- PB7
  */
@@ -102,7 +114,7 @@ void LSM303DLHC_SetAccelerometerScale(LSM303DLHC_Accel_Scale_Type scale) {
 //	tempval |= (1 << 7);		// 0: continuous update, 1: output registers not updated until MSB and LSB have been read
 //	tempval |= (1 << 6);		// 0: data LSB @ lower address, 1: data MSB @ lower address
 	tempval |= (scale << 4);	// Full-scale selection.
-	tempval |= (1 << 3);		// 0: high-resolution disable, 1: high-resolution enable
+	tempval |= (1 << 3);		// 0: high-resolution disable (10-bit), 1: high-resolution enable (12-bit)
 //	tempval |= (1 << 0);		// 0: 4-wire interface, 1: 3-wire interface
 
 	LSM303DLHC_WriteRegister(LSM303DLHC_ACCEL_ADDRESS, LSM303DLHC_CTRL_REG4_A, tempval);
@@ -237,7 +249,8 @@ int16_t LSM303DLHC_GetTemperature(void) {
 
 	LSM303DLHC_ReadRegisters(LSM303DLHC_MAG_ADDRESS, LSM303DLHC_TEMP_OUT_H_M, data, 2);
 
-	return (int16_t)((data[0] << 4) | (data[1] >> 4));
+	// 12-bit ADC. 20 degrees offset picked up empirically.
+	return (((int16_t)((data[0] << 8) | data[1]) >> 4) / 8) + 20;
 }
 
 // ---------------------------------------------
@@ -249,10 +262,10 @@ void LSM303DLHC_GetAccelerometer(LSM303DLHC_TypeDef *lsm303dlhc) {
 
 	LSM303DLHC_ReadRegisters(LSM303DLHC_ACCEL_ADDRESS, LSM303DLHC_OUT_X_L_A | LSM303DLHC_MULTIPLE_FLAG, data, 6);
 
-	// LSB first
-	lsm303dlhc->AccelX_ADC = (int16_t)((data[1] << 8) | data[0]);
-	lsm303dlhc->AccelY_ADC = (int16_t)((data[3] << 8) | data[2]);
-	lsm303dlhc->AccelZ_ADC = (int16_t)((data[5] << 8) | data[4]);
+	// LSB first, shift values to 6 bits for 10-bit resolution and to 4 bits for 12-bit resolution.
+	lsm303dlhc->AccelX_ADC = (int16_t)((data[1] << 8) | data[0]) >> 4;
+	lsm303dlhc->AccelY_ADC = (int16_t)((data[3] << 8) | data[2]) >> 4;
+	lsm303dlhc->AccelZ_ADC = (int16_t)((data[5] << 8) | data[4]) >> 4;
 
 	lsm303dlhc->AccelX = (float)lsm303dlhc->AccelX_ADC * accel_sensitivity;
 	lsm303dlhc->AccelY = (float)lsm303dlhc->AccelY_ADC * accel_sensitivity;
@@ -268,7 +281,7 @@ void LSM303DLHC_GetMagnetometer(LSM303DLHC_TypeDef *lsm303dlhc) {
 
 	LSM303DLHC_ReadRegisters(LSM303DLHC_MAG_ADDRESS, LSM303DLHC_OUT_X_H_M, data, 6);
 
-	// MSB first
+	// MSB first (12-bit ADC)
 	lsm303dlhc->MagX_ADC = (int16_t)((data[0] << 8) | data[1]);
 	lsm303dlhc->MagY_ADC = (int16_t)((data[2] << 8) | data[3]);
 	lsm303dlhc->MagZ_ADC = (int16_t)((data[4] << 8) | data[5]);
--- README.md
+++ README.md
@@ -21,7 +21,7 @@ IDE - Atollic TrueSTUDIO for STM32 9.3.0
 - **BMP280** (I2C)
 - **PCF8574T** (I2C)
 - **EEPROM/24C64** (I2C)
-- **LSM303DLHC** (I2C) Under development.
+- **LSM303DLHC** (I2C)
 - **LCD1602** (8-bit/4-bit/PCF8574T)
 - **LED_DISPLAY** (74HC595)
 - **HCSR04**
